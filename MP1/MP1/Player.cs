fe7944d49a24a8d54a9bbb0857587bb492196f3b
Cezary Wojcieszuk <s22798@pjwstk.edu.pl>
2023-03-02 13:28:11 +0100

    init

---
+++ MP1/MP1/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP1
+{
+    public class Player
+    {
+        private string _name;
+        private DateTime _dateOfBirth;
+        private string? _gender = String.Empty;
+
+        public Player(string name, DateTime dateOfBirth, string? gender = null)
+        {
+            _name = name;
+            _dateOfBirth = dateOfBirth;
+            _gender = gender;
+
+            addPlayer(this);
+        }
+
+        private static List<Player> players = new List<Player>();
+
+        private string getPlayerData()
+        {
+            return $"Player: {this._name} date of birth: {this._dateOfBirth} {(_gender == null ? "" : $"gender: {this._gender}")}";
+        }
+
+        private static void addPlayer(Player player)
+        {
+            players.Add(player);
+        }
+
+        private static void removeGracz(Player player)
+        {
+            players.Remove(player);
+        }
+
+        public static void showExtent()
+        {
+            Console.WriteLine($"Extent of the class: {typeof(Player).Name} \n");
+
+            foreach(Player player in players)
+            {
+                Console.WriteLine(player.getPlayerData());
+            } 
+        }
+    }
+}
---
+++ MP1/MP1/Program.cs
@@ -0,0 +1,7 @@
+﻿using MP1;
+
+Player gracz = new Player("Marek", DateTime.Now, "Man");
+Player gracz1 = new Player("Arek", DateTime.Now);
+Player gracz2 = new Player("Jarek", DateTime.Now);
+
+Player.showExtent();
