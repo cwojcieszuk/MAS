96d3071252412db119e7e9b320a72704ea9c7542
s22798 <s22798@pjwstk.edu.pl>
2023-03-09 13:15:16 +0100

    final commit

--- MP1/MP1/Program.cs
+++ MP1/MP1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MP1;
+const string _fileName = @"C:\Users\s22798\Desktop\MAS\MP1\MP1\players.json";
 
 Player player = new Player("Marek", new DateTime(2001, 12, 01), new List<string>(), "Man");
 Player player1 = new Player("Arek", new DateTime(2003, 12, 01), new List<string>{ "Man United", "Real Madrid" });
@@ -6,7 +7,10 @@ Player player2 = new Player("Jarek", new DateTime(1999, 12, 01), new List<string
 Player player3 = new Player("Marcin", new DateTime(2006, 12, 01), new List<string>{ "Liverpool" });
 
 player2.AddToFavouriteTeams("Bayern Monachium");
-player3.AddToFavouriteTeams(new []{"PSG", "Real Madrid"});
+player3.AddToFavouriteTeams(new[] { "PSG", "Real Madrid" });
+
+Player.WriteExtent(_fileName);
+Player.ReadExtent(_fileName);
 
 Player.ShowExtent();
 Player.ShowYoungestPlayer();
